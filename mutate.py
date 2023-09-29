7b383846408fb6ee8f8222559fe6bd84c4759b7d
MahdiGMK <mbahramiand@gmail.com>
2023-09-29 06:10:41 +0330

    genetics done?

--- mutate.py
+++ mutate.py
@@ -1,6 +1,12 @@
 import os , json , random
-import learn
 import random
+
+def numberInRange(min , max):
+    if(min == max) : return min
+    return random.uniform(min , max)
+
+def randHash() -> str:
+    return "%016x" % random.getrandbits(64)
 
 def filter(bestCut , randChance) :
     with open('scores.json' , 'r') as fl :
@@ -11,7 +17,7 @@ def filter(bestCut , randChance) :
     idx = 0
     for id in sorted_ids :
         idx += 1
-        exist = idx <= bestCut * (len(sorted_ids) - 2) or random.random() <= randChance
+        exist = idx <= bestCut * len(sorted_ids) or random.random() <= randChance
         if not exist :
             scores.pop(id)
             os.remove(id)
@@ -30,8 +36,6 @@ def mutate(mutation , count) :
         with open(key, 'r') as fl :
             genomes.append(json.load(fl))
     
-    G1CUT = 1 - mutation
-    G0CUT = G1CUT / 2
     for i in range(count) : 
         i0 = random.choice(genomes)
         i1 = random.choice(genomes)
@@ -39,10 +43,26 @@ def mutate(mutation , count) :
         result = {}
         for p in parameters :
             rnd = random.random()
-            if rnd < G0CUT :
+            if rnd <= .5 :
                 result[p] = i0[p]
-            elif rnd < G1CUT :
-                result[p] = i1[p]
             else :
-                result[p] = learn.numberInRange(parameters[p]['min'] , parameters[p]['max'])
+                result[p] = i1[p]
+            
+            if random.random() < mutation :
+                result[p] += random.uniform(-result[p] / 10 , result[p] / 10)
+                
         mutatedGenomes.append(result)
+    folder_path = 'genomes'
+    # for filename in os.listdir(folder_path):
+    #     file_path = os.path.join(folder_path, filename)
+    #     if filename.endswith('.json') and os.path.isfile(file_path):
+    #         os.remove(file_path)
+    for m in mutatedGenomes : 
+        file_name = str(randHash()) + ".json"
+        file_path = os.path.join(folder_path, file_name)
+        with open(file_path, 'w') as fl:
+            json.dump(m, fl, indent=4)
+
+
+filter(7/30 , 3/30)
+mutate(.1 , 15)
--- solver.sh
+++ solver.sh
@@ -1,9 +1,15 @@
 COUNTER=1
-# while [ true ]
-# do
-echo $COUNTER
-pypy3 learn.py & pypy3 learn.py & pypy3 learn.py & pypy3 learn.py & pypy3 learn.py & pypy3 learn.py
-wait
-python mutate.py
-((COUNTER++))
-# done
+while [ true ]
+do
+    echo ""
+    echo "ROUND $COUNTER"
+    echo ""
+    rm scores.json
+    pypy3 learn.py & pypy3 learn.py & pypy3 learn.py & pypy3 learn.py & pypy3 learn.py & pypy3 learn.py
+    wait
+    mkdir genomes_$COUNTER
+    cp genomes/* genomes_$COUNTER
+    cp scores.json genomes_$COUNTER
+    python mutate.py
+    ((COUNTER++))
+done
